c5d0b11a4ab0e16b72521cc9f96f7ba03422ccf0
menocrat228 <Tolikpolik7@gmail.com>
2025-12-26 09:59:29 +0200

    Added interaction with the user

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,12 @@
-﻿int a = Convert.ToInt32(Console.ReadLine());
+﻿Console.WriteLine("Enter three numbers:");
+int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
 int c = Convert.ToInt32(Console.ReadLine());
 int z = b;
 b = c;
 c = a;
 a = z;
+Console.WriteLine("Numbers after swapping:");
 Console.WriteLine($"{a} {b} {c}");
 
 //Емельяненко
