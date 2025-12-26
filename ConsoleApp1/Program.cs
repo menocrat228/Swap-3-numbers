39b020bb7689f100033a11d736be95358c9d0d27
menocrat228 <Tolikpolik7@gmail.com>
2025-12-26 10:06:26 +0200

    Added comments to the code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,18 @@
-﻿Console.WriteLine("Enter three numbers:");
+﻿//Enter three numbers and swap their values: a to b, b to c, c to a.
+
+// Read three integers from user input
+Console.WriteLine("Enter three numbers:");
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
 int c = Convert.ToInt32(Console.ReadLine());
+
+// Swap the values: a -> b, b -> c, c -> a
 int z = b;
 b = c;
 c = a;
 a = z;
+
+// Output the swapped values
 Console.WriteLine("Numbers after swapping:");
 Console.WriteLine($"{a} {b} {c}");
 
