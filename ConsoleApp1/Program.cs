4b15d1a3fbba0ef119b82c5ad87a80dcdf209cef
menocrat228 <Tolikpolik7@gmail.com>
2025-12-26 09:22:34 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int z = b;
+b = c;
+c = a;
+a = z;
+Console.WriteLine($"{a} {b} {c}");
+
+//Емельяненко
